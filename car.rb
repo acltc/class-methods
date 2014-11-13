268c6add3c7b7ab1a3c234e71bd4d438f98d3f6a
Jay Wengrow <jaywngrw@gmail.com>
2014-11-13 17:42:47 -0600

    introducing class methods

--- car.rb
+++ car.rb
@@ -12,4 +12,12 @@ class Car
   def beep
     `say "beep" `
   end
+
+  def self.make_a_honda_accord(color, year)
+    new(:year => year, :make => "Honda", :model => "Accord", :color => color)
+  end
+
+  def self.find_from_database_by_id(id)
+    # blah blah
+  end
 end
